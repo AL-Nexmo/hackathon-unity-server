51451fb47daa281fa6e025b4b3f05ac19a555b4c
jchapman <jamiechapman8462@gmail.com>
2019-11-14 19:06:09 +0000

    respected REST

--- src/main/java/com/vonage/dungeon/votes/controllers/NccoController.java
+++ src/main/java/com/vonage/dungeon/votes/controllers/NccoController.java
@@ -7,7 +7,7 @@ import java.util.Collection;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +18,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RequestMapping()
 public class NccoController {
 
-    @PostMapping(value = "/ncco", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(value = "/ncco", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Collection> returnNcco() {
 
         ObjectMapper mapper = new ObjectMapper();
