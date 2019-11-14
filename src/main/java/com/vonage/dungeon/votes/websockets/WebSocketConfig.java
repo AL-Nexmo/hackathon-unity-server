4dd8171aa6390ef6abdfc6efc587f28116ed5760
Anthony Landau <anthony.landau@vonage.com>
2019-11-14 16:28:01 +0000

    Fix application so it starts

--- src/main/java/com/vonage/dungeon/votes/websockets/WebSocketConfig.java
+++ src/main/java/com/vonage/dungeon/votes/websockets/WebSocketConfig.java
@@ -3,6 +3,7 @@ package com.vonage.dungeon.votes.websockets;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 
@@ -15,4 +16,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         config.enableSimpleBroker("/socket");
     }
 
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
 }
--- src/main/java/com/vonage/dungeon/votes/websockets/WebSocketController.java
+++ src/main/java/com/vonage/dungeon/votes/websockets/WebSocketController.java
@@ -1,12 +1,14 @@
 package com.vonage.dungeon.votes.websockets;
 
 import com.vonage.dungeon.votes.domain.VoteSummary;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WebSocketController {
 
+    @MessageMapping("/socket")
     @SendTo("/socket")
     public VoteSummary sendMessage(VoteSummary message) {
         return message;
