57b6c2aaf337570cfa5147d254380b92b9f938d3
jchapman <jamiechapman8462@gmail.com>
2019-11-14 19:13:35 +0000

    hardcoded NCCO in controller as wasn't loading when deployed to heroku

--- src/main/java/com/vonage/dungeon/votes/controllers/NccoController.java
+++ src/main/java/com/vonage/dungeon/votes/controllers/NccoController.java
@@ -1,17 +1,10 @@
 package com.vonage.dungeon.votes.controllers;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Collection;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @RestController
@@ -19,17 +12,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class NccoController {
 
     @GetMapping(value = "/ncco", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Collection> returnNcco() {
+    public ResponseEntity<String> returnNcco() {
 
-        ObjectMapper mapper = new ObjectMapper();
-        InputStream is = String.class.getResourceAsStream("/ncco.json");
-        try {
-            Collection test = mapper.readValue(is, Collection.class);
-            System.out.println(test);
-            return ResponseEntity.ok(test);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        String blah = "[\n" +
+            "  {\n" +
+            "    \"action\": \"talk\",\n" +
+            "    \"text\": \"Please enter a digit\",\n" +
+            "    \"bargeIn\": true\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"action\": \"input\",\n" +
+            "    \"eventUrl\": [\"http://dungeon-one-hack-server.herokuapp.com/dtmf\"],\n" +
+            "    \"timeOut\": 10,\n" +
+            "    \"maxDigits\": 1\n" +
+            "  }\n" +
+            "]";
+
+        return ResponseEntity.ok(blah);
     }
 }
